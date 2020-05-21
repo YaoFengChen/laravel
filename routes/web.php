2538784d91717b7b4a11210fe555eb8c46789db2
Yao-Feng,Chen <guy414548@gmail.com>
2020-05-21 18:18:27 +0800

    swagger功能 (#4)

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('/member/{id}', 'MemberController@getMember');
 Route::get('/members', 'MemberController@getMembers');
+
+Route::get('/api/swagger', 'SwaggerController@json');
+Route::get('/swagger', 'SwaggerController@index');
