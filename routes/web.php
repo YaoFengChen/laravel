51457f507e2526a8d87a4a015269e77bb3f9c038
Yao-Feng,Chen <guy414548@gmail.com>
2020-07-01 18:05:58 +0800

    member (#5)
    member 新增註解

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('/member/{id}', 'MemberController@getMember');
 Route::get('/members', 'MemberController@getMembers');
+Route::post('/member', 'MemberController@createMember');
 
 Route::get('/api/swagger', 'SwaggerController@json');
 Route::get('/swagger', 'SwaggerController@index');
