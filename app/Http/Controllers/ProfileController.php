8f2dd82dfaec6fc6c2f561337dc2e5304706c11d
Yao-Feng,Chen <guy414548@gmail.com>
2021-03-18 15:04:12 +0800

    Release (#26)
    swagger 文檔更新
    新增 security機制

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,12 +8,8 @@ class ProfileController extends Controller
      * @OA\Get(
      *     path="/profile",
      *     summary="取得登入的個人資料",
-     *     @OA\Parameter(
-     *     name="token",
-     *     in="query",
-     *     description="jwt token",
-     *     required=true,
-     *     ),
+     *     tags={"member"},
+     *     security={ {"Authorization": {} }},
      *     @OA\Response(response="200", description="成功"),
      *     @OA\Response(response="402",description="授權失敗"),
      * )
